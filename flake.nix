3df5fa9be35a664fc7f66819bd3bf3f2b1815b9d
diniamo <55629891+diniamo@users.noreply.github.com>
2024-08-20 12:34:54 +0000

    Initial commit
    Initial commit
    
    Add license

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs.nixpkgs.url = "nixpkgs/master";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+    };
+  in {
+    devShells.${system}.comfyui = pkgs.mkShell {
+      packages = [
+        (pkgs.python3.withPackages (pythonPackages:
+          with pythonPackages; [
+            torch
+            torchsde
+            torchvision
+            torchaudio
+            einops
+            transformers
+            tokenizers
+            sentencepiece
+            safetensors
+            aiohttp
+            pyyaml
+            pillow
+            tqdm
+            psutil
+
+            kornia
+            # spandrel
+            soundfile
+          ]))
+      ];
+    };
+
+    nixConfig = {
+      extra-substituters = ["https://cuda-maintainers.cachix.org"];
+      extra-trusted-public-keys = ["cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="];
+    };
+  };
+}
